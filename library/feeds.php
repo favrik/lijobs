d90a9bf3fb3e50d189da0c21f377de1345c1cc02
Favio Manriquez Leon <favio@favrik.com>
2014-10-15 22:27:48 -0700

    Update oDesk feed URL

--- library/feeds.php
+++ library/feeds.php
@@ -86,7 +86,7 @@ class FSFeed extends SaikyoFeed {
 class oDeskFeed extends SaikyoFeed {
 
     public function init() {
-        $this->url = 'http://www.odesk.com/jobs/rss?q=&t=Hourly&c1=Web+Development';
+        $this->url = 'https://www.odesk.com/jobs/rss?t[]=0&dur[]=0&dur[]=1&dur[]=13&dur[]=26&dur[]=none&wl[]=10&wl[]=30&wl[]=none&tba[]=0&tba[]=1-9&tba[]=10-&exp[]=1&exp[]=2&exp[]=3&amount[]=Min&amount[]=Max&sortBy=s_ctime+desc';
         $this->id  = 'odesk';
         $this->refresh = 3600;
     }
